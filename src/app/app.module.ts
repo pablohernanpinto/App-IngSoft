6b47add75dd18a89098869f7e47e97fa23ce7e58
Pablo Pinto <pablohernanpinto@gmail.com>
2023-10-12 23:02:00 -0300

    Subir archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
